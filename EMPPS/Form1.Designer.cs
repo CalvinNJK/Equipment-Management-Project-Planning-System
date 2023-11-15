d46f5ba5a208f9951d8874a799dd5579e4344876
CalvinNJK <87193669+CalvinNJK@users.noreply.github.com>
2023-11-15 16:08:04 +0800

    Created Menu Buttons, Folders & Classes
    Project Folders
    - Equipment Management
    - Project Planning
    
    Classes
    - Equipment
    - Project
    
    Form
    - Button for Equipment Management
    - Button for Project Planning

--- EMPPS/Form1.Designer.cs
+++ EMPPS/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_em = new System.Windows.Forms.Button();
+            this.button_pp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_em
+            // 
+            this.button_em.Location = new System.Drawing.Point(268, 112);
+            this.button_em.Name = "button_em";
+            this.button_em.Size = new System.Drawing.Size(260, 23);
+            this.button_em.TabIndex = 0;
+            this.button_em.Text = "Equipment Management";
+            this.button_em.UseVisualStyleBackColor = true;
+            this.button_em.Click += new System.EventHandler(this.button_em_Click);
+            // 
+            // button_pp
+            // 
+            this.button_pp.Location = new System.Drawing.Point(268, 172);
+            this.button_pp.Name = "button_pp";
+            this.button_pp.Size = new System.Drawing.Size(245, 23);
+            this.button_pp.TabIndex = 1;
+            this.button_pp.Text = "Project Planning";
+            this.button_pp.UseVisualStyleBackColor = true;
+            this.button_pp.Click += new System.EventHandler(this.button_pp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button_pp);
+            this.Controls.Add(this.button_em);
+            this.Name = "Form1";
+            this.Text = "Equipment Management & Project Planning Systen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_em;
+        private System.Windows.Forms.Button button_pp;
     }
 }
 
--- EMPPS/Form1.cs
+++ EMPPS/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EMPPS.Project_Planning;
+using EMPPS.Equipment_Management;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,26 @@ namespace EMPPS
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_em_Click(object sender, EventArgs e)
+        {
+            var em_win = new EM_Main();
+            em_win.Show();
+            this.Hide();
+            
+        }
+
+        private void button_pp_Click(object sender, EventArgs e)
+        {
+            var pp_win = new PP_Main();
+            pp_win.Show();
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
