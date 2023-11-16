40093700a03f56a3e684ea5d88b0f5549b7d47b8
jiawen0101 <Jwjiawen1314@gmail.com>
2023-11-17 00:56:58 +0800

    Create EquipmentCost  Entity

---
+++ EMPPS/Classes/EquipmentCost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPPS.Classes
+{
+    internal class EquipmentCost
+    {
+        private string e_cost;
+        private string e_duration;
+
+        public string E_Duration
+        {
+            get { return e_duration; }
+            set { e_duration = value; }
+
+        }
+
+        public void EquipmentCost(string e_cost, string e_duration)
+        {
+            return (e_cost * e_duration);
+        }
+    }
+}
