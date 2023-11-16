667fd0839d2176edc3697f2853575d3c8c31f1d7
CalvinNJK <87193669+CalvinNJK@users.noreply.github.com>
2023-11-16 09:12:52 +0800

    Done Equipment & Category Classes

---
+++ EMPPS/Classes/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPPS.Classes
+{
+    internal class Category
+    {
+        // Variables
+        private int c_Index;
+        private string c_Name;
+
+        // Setter and Getter
+        public int C_Index
+        {  
+            get { return c_Index; } 
+            set { c_Index = value; }
+        }
+
+        public string C_Name
+        {
+            get { return c_Name; }
+            set { c_Name = value; }
+        }
+
+        // Default Constructor
+        public Category(int cc_Index, string cc_Name)
+        {
+            this.c_Index = cc_Index;
+            this.c_Name = cc_Name;
+        }
+    }
+}
--- EMPPS/Classes/Equipment.cs
+++ EMPPS/Classes/Equipment.cs
@@ -8,5 +8,55 @@ namespace EMPPS.Classes
 {
     internal class Equipment
     {
+        // Variables 
+        private string e_Id;
+        private string e_Name;
+        private string e_Desc;
+        private double e_Cost;
+        private string e_Category;
+        private int e_Status;
+
+        // Setter & Getter
+        public string E_Id
+        {
+            get { return e_Id; }
+            set { e_Id = value; }
+        }
+        public string E_Name
+        {
+            get { return e_Name; }
+            set { e_Name = value; }
+        }
+        public string E_Desc
+        {
+            get { return e_Desc; }
+            set { e_Desc = value; }
+        }
+        public double E_Cost
+        {
+            get { return e_Cost; }
+            set { e_Cost = value; }
+        }
+        public string E_Category
+        {
+            get { return e_Category; }
+            set { e_Category = value; }
+        }
+        public int E_Status
+        {
+            get { return e_Status; }
+            set { e_Status = value; }
+        }
+
+        // Default Constructor
+        public Equipment(string id, string name, string desc, double cost, string cat, int status) 
+        { 
+            this.e_Id = id;
+            this.e_Name = name;
+            this.e_Desc = desc;
+            this.e_Cost = cost;
+            this.e_Category = cat;
+            this.e_Status = status;
+        }
     }
 }
