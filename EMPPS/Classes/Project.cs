94fcdcc494b7fcff75d73bdf4a89e1523149e91f
jiawen0101 <Jwjiawen1314@gmail.com>
2023-11-17 00:58:15 +0800

    Update Project Entity

--- EMPPS/Classes/Project.cs
+++ EMPPS/Classes/Project.cs
@@ -8,5 +8,56 @@ namespace EMPPS.Classes
 {
     internal class Project
     {
+        // Variables 
+        private string p_id;
+        private string p_name;
+        private string p_desc;
+        private string p_leaderId;
+        private string p_duration;
+        private string p_budget;
+
+        // Setter & Getter
+        public string P_ID
+        {
+            get { return p_id; }
+            set { p_id = value; }
+        }
+        public string P_Name
+        {
+            get { return p_name; }
+            set { p_name = value; }
+        }
+        public string P_Desc
+        {
+            get { return p_desc; }
+            set { p_desc = value; }
+        }
+        public string P_LeaderId
+        {
+            get { return p_leaderId; }
+            set { p_leaderId = value; }
+        }
+        public string P_Duration
+        {
+            get { return p_duration; }
+            set { p_duration = value; }
+        }
+
+        public string P_Budget
+        {
+            get { return p_budget; }
+            set { p_budget = value; }
+        }
+
+        // Default Constructor
+        public Project(string id, string name, string desc, string leaderid, string duration)
+        {
+            this.p_id = id;
+            this.p_name = name;
+            this.p_desc = desc;
+            this.p_leaderId = leaderid;
+            this.p_duration = duration;
+            this.p_budget = null;
+        }
     }
 }
