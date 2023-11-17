e4ddd1573812c05a0f23daf7db258136aa9e60d9
jiawen0101 <Jwjiawen1314@gmail.com>
2023-11-17 18:01:04 +0800

    Change to EquipmentUsed Class from EquipmentCost

---
+++ EMPPS/Classes/EquipmentUsed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMPPS.Classes
+{
+    internal class EquipmentUsed
+    {
+        private string eu_id;
+        private string eu_cost;
+        private string eu_duration;
+
+        public string EU_ID
+        {
+            get { return eu_id; }
+            set { eu_id = value; }
+        }
+
+        public string EU_Cost
+        {
+            get { return eu_cost; }
+            set { eu_cost = value; }
+        }
+
+        public string EU_Duration
+        {
+            get { return e_duration; }
+            set { e_duration = value; }
+
+        }
+    }
+}
