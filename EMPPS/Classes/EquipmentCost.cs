0c741fd77d07379321e3734cc55bf112abbb4129
jiawen0101 <Jwjiawen1314@gmail.com>
2023-11-17 17:48:59 +0800

    Delete Equipment Cost Function

--- EMPPS/Classes/EquipmentCost.cs
+++ EMPPS/Classes/EquipmentCost.cs
@@ -17,10 +17,5 @@ namespace EMPPS.Classes
             set { e_duration = value; }
 
         }
-
-        public double EquipmentCost(string e_cost, string e_duration)
-        {
-            return (Double.Parse(e_cost) * Double.Parse(e_duration));
-        }
     }
 }
