e964f00a9c99d0f8667dd0a7cf01a21d8d505fb7
CalvinNJK <87193669+CalvinNJK@users.noreply.github.com>
2023-11-18 12:24:07 +0800

    Checkpoint for Equipment Managed
    Done
    - Dashboard

--- EMPPS/Equipment_Management/FormEM_Main.cs
+++ EMPPS/Equipment_Management/FormEM_Main.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,12 @@ namespace EMPPS.Equipment_Management
 {
     public partial class FormEM_Main : Form
     {
+        // Variable for PieChart
+        private int s_OnLoan = 0;
+        private int s_Available = 0;
+        private int s_Damaged = 0;
+
+
         public FormEM_Main()
         {
             InitializeComponent();
@@ -23,7 +30,7 @@ namespace EMPPS.Equipment_Management
 
         private void EM_Main_Load(object sender, EventArgs e)
         {
-            LoadListView_eq();
+            LoadReloadViews();
         }
 
 
@@ -39,19 +46,22 @@ namespace EMPPS.Equipment_Management
         {
             var ea_win = new FormEquipmentAdd();
             ea_win.ShowDialog();
-            listView_eq.Items.Clear();
-            LoadListView_eq();
+
+
+            LoadReloadViews();
         }
         private void editCategToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var cc_win = new FormCategory();
             cc_win.ShowDialog();
+            LoadReloadViews();
         }
 
         private void addCategoryToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             var ca_win = new FormCategoryAdd();
             ca_win.ShowDialog();
+            LoadReloadViews();
         }
 
 
@@ -95,7 +105,7 @@ namespace EMPPS.Equipment_Management
         //
         // ListView
         //
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void listView_eq_SelectedIndexChanged(object sender, EventArgs e)
         {
             // Toggle enable when item selected
             if (listView_eq.SelectedItems.Count == 0)
@@ -108,9 +118,6 @@ namespace EMPPS.Equipment_Management
                 b_eModify.Enabled = true;
                 b_eDelete.Enabled = true;
             }
-
-
-            
         }
 
         private void listView_eq_MouseDoubleClick(object sender, EventArgs e)
@@ -129,16 +136,18 @@ namespace EMPPS.Equipment_Management
         {
             var ea_win = new FormEquipmentAdd();
             ea_win.ShowDialog();
-            listView_eq.Items.Clear();
-            LoadListView_eq();
+
+
+            LoadReloadViews();
         }
 
         private void b_eModify_Click(object sender, EventArgs e)
         {
             var em_win = new FormEquipmentModify(listView_eq.SelectedItems[0].Text.ToString());
             em_win.ShowDialog();
-            listView_eq.Items.Clear();
-            LoadListView_eq();
+
+
+            LoadReloadViews();
             b_eModify.Enabled = false;
             b_eDelete.Enabled = false;
 
@@ -164,8 +173,8 @@ namespace EMPPS.Equipment_Management
                 }
             }
 
-            listView_eq.Items.Clear();
-            LoadListView_eq();
+
+            LoadReloadViews();
             b_eModify.Enabled = false;
             b_eDelete.Enabled = false;
         }
@@ -173,10 +182,79 @@ namespace EMPPS.Equipment_Management
 
 
         //
-        // Load all the Equipments into Table
+        // Load/Reload all the views
         //
-        private void LoadListView_eq()
+        private void LoadReloadViews()
         {
+            //
+            // FOR DASHBOARD
+            //
+            s_Available = 0;
+            s_Damaged = 0;
+            s_OnLoan = 0;
+
+            // Clear the Pie Charts
+            chart1.Series["s1"].Points.Clear();
+            chart2.Series["s1"].Points.Clear();
+
+
+            // Status Pie Chart
+            foreach (var item in FileHandling.equipmentList)
+            {
+                if (item.E_Status == 0)
+                {
+                    s_Available += 1;
+                }
+                else if (item.E_Status == 1)
+                {
+                    s_OnLoan += 1;
+                }
+                else if (item.E_Status == 2)
+                {
+                    s_Damaged += 1;
+                }
+            }
+            chart1.Series["s1"].IsValueShownAsLabel = true;
+            chart1.Series["s1"].Points.AddXY("Available", s_Available.ToString());
+            chart1.Series["s1"].Points.AddXY("On Loan", s_OnLoan.ToString());
+            chart1.Series["s1"].Points.AddXY("Damaged", s_Damaged.ToString());
+
+
+            // Category Pie Chart
+            var query = FileHandling.equipmentList.SelectMany(x => x.E_Category.ToString())
+                .GroupBy(s => s)
+                .Select(g => new { Index = g.Key, Count = g.Count() });
+
+            chart2.Series["s1"].IsValueShownAsLabel = true;
+
+            foreach (var result in query)
+            {
+                Console.WriteLine("Index: {0}, Count: {1}", result.Index, result.Count);
+
+                foreach (var item in FileHandling.categoryList)
+                {
+                    if (string.Compare(result.Index.ToString(), item.C_Index.ToString()) == 0)
+                    {
+                        chart2.Series["s1"].Points.AddXY(item.C_Name, result.Count.ToString());
+                    }
+                }
+            }
+
+            label_TotalEquipment.Text = FileHandling.equipmentList.Count().ToString();
+            label_Available.Text = s_Available.ToString();
+            label_OnLoan.Text = s_OnLoan.ToString();
+            label_Damaged.Text = s_Damaged.ToString();
+            label_TotalCat.Text = FileHandling.categoryList.Count().ToString();
+
+
+
+
+            //
+            // FOR EQUIPMENT INVENTORY
+            //
+
+            // Update the ListView_eq
+            listView_eq.Items.Clear();
             foreach (var item in FileHandling.equipmentList)
             {
                 ListViewItem lvItem = new ListViewItem(item.E_Id);
@@ -195,8 +273,15 @@ namespace EMPPS.Equipment_Management
                 lvItem.SubItems.Add(item.E_Status.ToString());
                 listView_eq.Items.Add(lvItem);
             }
+
+            // Update the labels
+            label_TotalEquipment2.Text = FileHandling.equipmentList.Count().ToString();
+
         }
 
-        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
